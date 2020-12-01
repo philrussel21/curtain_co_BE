dbb4679907a1c176fae00b76d82a4114a83f8551
philrussel21 <phil.antiporda21@gmail.com>
2020-12-01 10:43:53 +1000

    inits User Schema

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// TODO - Validations
+const User = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    bcrypt: true
+  },
+  title: {
+    type: String,
+    // required: true
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true
+  },
+  companyName: {
+    type: String,
+    required: true
+  },
+  address1: {
+    type: String,
+    required: true
+  },
+  address2: {
+    type: String
+  },
+  suburb: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  postcode: {
+    type: Number,
+    required: true
+  }
+},
+  // adds createdAt and updatedAt attributes when saving
+  // to DB
+  { timestamps: true });
+
+// User.index({'email': 1})
+User.plugin(require('mongoose-bcrypt'));
+
+module.exports = mongoose.model('User', User);
