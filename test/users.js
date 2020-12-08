1fa683ab4d0aecdb5fd486c6184cb2d361ff2745
philrussel21 <phil.antiporda21@gmail.com>
2020-12-08 15:31:12 +1000

    Admin role integration tests completed

--- test/users.js
+++ test/users.js
@@ -6,11 +6,12 @@ const agent = chai.request.agent(app);
 const { expect } = chai;
 const testData = require('./data.json');
 const User = require('../models/user');
+const user = require('../models/user');
+const { logout } = require('../controllers/auth_controller');
 const accountRoute = '/api/account';
 const userRoute = '/api/users';
 
 
-const admin = testData.find(user => user.role === 'admin');
 
 before(done => {
   mongoose.connection.db.dropCollection('users', () => {
@@ -23,7 +24,12 @@ after(done => {
   done();
 });
 
-describe('ADMIN Actions', () => {
+describe('ADMIN Role Actions', () => {
+
+  const admin = testData.find(user => user.role === 'admin');
+  let user1Id = null;
+
+
   // Login as admin
   it('should login as admin', (done) => {
     authUser(admin, done);
@@ -31,7 +37,7 @@ describe('ADMIN Actions', () => {
 
   // GET all users
   it('should return all users', (done) => {
-    agent.get("/api/users")
+    agent.get(`${userRoute}`)
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res.body).to.be.an('array');
@@ -41,43 +47,81 @@ describe('ADMIN Actions', () => {
         done();
       });
   });
+
+  // GET one user
+  it('should show profile of one user', (done) => {
+    agent.get(`${userRoute}/${user1Id}`)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body).to.be.an('object');
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  // PATCH user details
+  it('should update one user profile', (done) => {
+    const user1 = {
+      fullName: "Gandalf the White"
+    };
+
+    agent.patch(`${userRoute}/${user1Id}`)
+      .type('form')
+      .send(user1)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.fullName).to.equal(user1.fullName);
+        done();
+      });
+  });
+
+  // DELETE user
+  it('should delete one user', (done) => {
+    let deletedUser = null;
+    agent.delete(`${userRoute}/${user1Id}`)
+      .then((res) => {
+        deletedUser = res.body;
+      })
+      .then(() => {
+        agent.get(`${userRoute}`)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.not.include(deletedUser);
+            done();
+          });
+      });
+
+  });
+
+  // ADMIN Logout
+  it('should logout', (done) => {
+    logOut(done);
+  });
+
 });
 
+describe('USER Role Actions', () => {
 
-// function setUpAdmin(user, done) {
-//   const testUser = {};
-//   testUser.email = 'bilbo@baggins';
-//   testUser.password = 'onering';
-//   testUser.title = 'Dr.';
-//   testUser.fullName = 'Bilbo Baggins';
-//   testUser.phone = 123456789;
-//   testUser.companyName = 'Precious';
-//   testUser.address1 = '21 Hobbitown';
-//   testUser.suburb = 'MiddleEarth';
-//   testUser.state = 'Queensland';
-//   testUser.postcode = 1234;
-//   testUser.role = 'admin';
+  const user = testData.find(user => user.email === 'andy@email');
 
-//   chai.request(app)
-//     .post("/api/account/register")
-//     .type('form')
-//     .send(testUser)
-//     .then(() => {
-//       return agent.post('/api/account')
-//         .type('form')
-//         .send({
-//           email: testUser.email,
-//           password: testUser.password
-//         })
-//         .then(() => {
-//           done();
-//         });
-//     })
-//     .catch(e => {
-//       console.log(e);
-//     });
+  // Login as user
+  it('should login as user', (done) => {
+    authUser(user, done);
+  });
 
-// }
+  // NOT GET all users
+  it('should NOT return all users', (done) => {
+    agent.get(`${userRoute}`)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(401);
+        done();
+      });
+  });
+
+});
 
 function authUser(user, done) {
   agent.post(`${accountRoute}`)
@@ -92,13 +136,22 @@ function authUser(user, done) {
     .catch(e => console.log(e));
 }
 
+function logOut(done) {
+  agent.get(`${accountRoute}/logout`)
+    .then(() => {
+      agent.close();
+      done();
+    });
+}
+
 function setUpUsers(users, done) {
   const promises = [];
   for (const user of users) {
     promises.push(User.create(user));
   }
   Promise.all(promises)
-    .then(() => {
+    .then((users) => {
+      user1Id = users[1]._id;
       done();
     });
 }
