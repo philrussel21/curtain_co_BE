ed898d7e18c5ffddf878c63b4a45b3f07f974e4f
philrussel21 <phil.antiporda21@gmail.com>
2020-12-13 15:49:02 +1000

    Adds middleware for protected routes

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const { checkAuthenticated, checkAdmin } = require('../middlewares/auth');;
 const { indexProducts, createProduct, showProduct, updateProduct, deleteProduct, changeProduct } = require('../controllers/products_controller');
 
-// consider PRODUCT CATEGORY/TYPE middleware
 
 router.get('/', indexProducts);
-router.post('/', createProduct);
+router.post('/', checkAuthenticated, checkAdmin, createProduct);
 router.get('/:id', showProduct);
-router.patch('/:id', changeProduct);
-router.delete('/:id', deleteProduct);
+router.patch('/:id', checkAuthenticated, checkAdmin, changeProduct);
+router.delete('/:id', checkAuthenticated, checkAdmin, deleteProduct);
 
 module.exports = router;
