1c7ba35e35178896fe1d20ae23ae6f73d65cd9de
philrussel21 <phil.antiporda21@gmail.com>
2020-12-13 15:46:26 +1000

    Inits parentModel- Product for Fabric, Track and Accessory

---
+++ models/product.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// TODO - validations
+const Product = new Schema({
+  name: { type: String },
+  colour: { type: String }
+},
+  {
+    discriminatorKey: 'category',
+    timestamps: true
+  });
+
+module.exports = mongoose.model('Products', Product);
