1b5adb8aad50e15d7790d53fd178fd171a8e147e
SimoSultan <simosultan2020@gmail.com>
2020-12-14 22:58:38 +1000

    started code review on BE and added markdown file

---
+++ code_review.md
@@ -0,0 +1,11 @@
+## BE Code Review
+
+##### By Simon Curran
+
+---
+
+### Server.js
+
+-   line 35: using short circuit logic here but there is no NODE_ENV var in the .env?
+-   secret key needs to be more complex...lol
+-
--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,51 +1,50 @@
-const passport = require('passport');
-const { addUser, getUserByEmail } = require('../utils/users');
+const passport = require("passport");
+const { addUser, getUserByEmail } = require("../utils/users");
 
 // TODO: validations
 
 async function register(req, res) {
-  const email = req.body.email;
-  try {
-    const existingEmail = await getUserByEmail(req);
-    if (existingEmail) {
-      return res.status(400).json({ message: "Existing Email" });
+    const email = req.body.email;
+    console.log(req);
+    try {
+        const existingEmail = await getUserByEmail(req);
+        if (existingEmail) {
+            return res.status(400).json({ message: "Existing Email" });
+        }
+        const newUser = await addUser(req);
+        return res.status(201).json(newUser);
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid Fields" });
     }
-    const newUser = await addUser(req);
-    return res.status(201).json(newUser);
-  } catch (error) {
-    return res.status(400).json({ message: "Invalid Fields" });
-  }
 }
 
-const authenticate = passport.authenticate('local', { failureFlash: true });
+const authenticate = passport.authenticate("local", { failureFlash: true });
 
 function login(req, res) {
-  authenticate(req, res, () => {
-
-
-    // REMEMBER ME COMP
-    if (req.body.remember) {
-      req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
-    } else {
-      req.session.cookie.maxAge = 3_600_000;
-    }
-    console.log('authenticated', req.user.email);
-    console.log('session object:', req.session);
-    console.log('req.user:', req.user);
-    res.status(200).json({ user: req.user, sessionID: req.sessionID });
-  });
+    authenticate(req, res, () => {
+        // REMEMBER ME COMP
+        if (req.body.remember) {
+            req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
+        } else {
+            req.session.cookie.maxAge = 3_600_000;
+        }
+        console.log("authenticated", req.user.email);
+        console.log("session object:", req.session);
+        console.log("req.user:", req.user);
+        res.status(200).json({ user: req.user, sessionID: req.sessionID });
+    });
 }
 
 function logout(req, res) {
-  req.logout();
-  console.log('logged out user');
-  console.log('session object:', req.session);
-  console.log('req.user:', req.user);
-  res.sendStatus(204);
+    req.logout();
+    console.log("logged out user");
+    console.log("session object:", req.session);
+    console.log("req.user:", req.user);
+    res.sendStatus(204);
 }
 
 module.exports = {
-  register,
-  login,
-  logout
+    register,
+    login,
+    logout,
 };
