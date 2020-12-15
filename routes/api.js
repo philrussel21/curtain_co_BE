327b5aa52803bc1e19154eaee80e17e4365a406a
philrussel21 <phil.antiporda21@gmail.com>
2020-12-15 14:05:33 +1000

    Adds middleware check if authenticated and if admin

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ const userRoutes = require('./user_routes');
 const authRoutes = require('./auth_routes');
 const consultRoutes = require('./consult_routes');
 const productRoutes = require('./product_routes');
-const { checkAuthenticated } = require('../middlewares/auth');
+const { checkAuthenticated, checkAdmin } = require('../middlewares/auth');
 const { singleUpload } = require('../config/file_upload');
 
 // HOME PAGE
@@ -22,7 +22,7 @@ router.use("/products", productRoutes);
 
 
 // Route for uploads to S3
-router.post('/upload', (req, res) => {
+router.post('/upload', checkAuthenticated, checkAdmin, (req, res) => {
   singleUpload(req, res, (err) => {
     if (err) {
       return res.status(422).json({ message: 'File Upload Error', error: err.message });
