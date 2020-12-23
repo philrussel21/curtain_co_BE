4d7f77896ccd7e43c162c1f098bc36f47d7e0540
philrussel21 <phil.antiporda21@gmail.com>
2020-12-23 12:37:18 +0930

    Updates attributes to match required fields and response from paypal

--- models/order.js
+++ models/order.js
@@ -1,20 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { UserSchema } = require('./user');
-const { ProductSchema } = require('./product');
-const { CollectionSchema } = require('./collection');
+
 
 // TODO - validations
 // TODO  - set _id from paypal's response: order_id 
 const Order = new Schema({
-  customer: UserSchema,
-  // products: [ProductSchema],
-  // collections: [CollectionSchema],
-  items: { type: Array, required: true },
+  _id: {
+    type: String,
+    required: true
+  },
+  // had to use referencing as embedding produces weird bugs
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: {
+    type: Array,
+    required: true
+  },
   // to be changed when admin process the orders?
   isProcessed: { type: Boolean, default: false },
   totalPrice: { type: Number },
-  paymentData: { type: Object, required: true }
+  // all other data from payPal
+  paymentData: {
+    type: Object,
+    required: true
+  }
 },
   { timestamps: true });
 
