039a993bb93cc9dd78aab23ec5ea9f61cce8e048
philrussel21 <phil.antiporda21@gmail.com>
2021-01-03 12:41:45 +0930

    Updates collection schema to have arrays of productIDs for track, fabric and accessory. Also adds tip attributes for each of the products

--- models/collection.js
+++ models/collection.js
@@ -7,9 +7,12 @@ const Collection = new Schema({
   description: { type: String },
   imgUrl: { type: String },
   price: { type: Number },
-  track: { type: Schema.Types.ObjectId, ref: 'Products' },
-  fabric: { type: Schema.Types.ObjectId, ref: 'Products' },
-  accessory: { type: Schema.Types.ObjectId, ref: 'Products' }
+  track: [{ type: Schema.Types.ObjectId, ref: 'Products' }],
+  trackTip: { type: String },
+  fabric: [{ type: Schema.Types.ObjectId, ref: 'Products' }],
+  fabricTip: { type: String },
+  accessory: [{ type: Schema.Types.ObjectId, ref: 'Products' }],
+  accessoryTip: { type: String }
 },
   { timestamps: true });
 
